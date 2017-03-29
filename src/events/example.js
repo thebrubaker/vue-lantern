389ad82e99817dc8d0bfb4747e7ffb54090923e4
Joel Brubaker <pk.joel@gmail.com>
2017-03-29 12:05:24 -0700

    - updated example event

--- src/events/example.js
+++ src/events/example.js
@@ -1,6 +1,7 @@
 export default {
   'test' (event, message) {
-    // Test this event in your console by typing `app.events.fire('example.test', 'There was an error testing the event [example.test]!')`
+    // Test this event in your console by typing:
+    // app.events.fire('example.test', 'This is a test!')
     error(message)
   }
 }
