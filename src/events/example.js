ef852ce3a06cb335f34c986e43dc714ae8b490b2
Joel Brubaker <pk.joel@gmail.com>
2017-03-28 17:26:36 -0700

    - removed app from test event

--- src/events/example.js
+++ src/events/example.js
@@ -1,5 +1,3 @@
-import app from 'app'
-
 export default {
   'test' (event, message) {
     // Test this event in your console by typing `app.events.fire('example.test', 'There was an error testing the event [example.test]!')`
