d931a003814bb6554c34e94c9599b4d47951e4fc
Joel Brubaker <pk.joel@gmail.com>
2017-03-29 18:36:38 -0700

    - refactored out index to a method - added update and delete methods

--- src/app/services/blueprint/drivers/algolia-driver.js
+++ src/app/services/blueprint/drivers/algolia-driver.js
@@ -11,18 +11,22 @@ export default class AlgoliaBlueprintDriver {
   }
 
   /**
+   * Return an index to the current model's location.
+   * @return {Index}  An algolia index.
+   */
+  index () {
+    return this.algolia.initIndex(this.location)
+  }
+
+  /**
    * Fetch data from the database by id.
    * @param  {string} id  The id of the model to be accessed.
    * @return {Object}  The model with the corresponding id.
    */
   fetch (id) {
     return new Promise((resolve, reject) => {
-      this.algolia.initIndex(this.location).getObject(id, (errorMessage, data) => {
-        if (errorMessage) {
-          reject(errorMessage)
-        } else {
-          resolve(data)
-        }
+      this.index().getObject(id, (error, data) => {
+        error ? reject(error) : resolve(data)
       })
     })
   }
@@ -34,12 +38,35 @@ export default class AlgoliaBlueprintDriver {
    */
   create (data) {
     return new Promise((resolve, reject) => {
-      this.algolia.initIndex(this.location).addObject(data, null, (errorMessage, data) => {
-        if (errorMessage) {
-          reject(errorMessage)
-        } else {
-          resolve(data)
-        }
+      this.index().addObject(data, null, (error, data) => {
+        error ? reject(error) : resolve(data)
+      })
+    })
+  }
+
+  /**
+   * Update a model in algolia.
+   * @param  {string} id  The id of the model
+   * @param  {Object} data  The data to be saved.
+   * @return {undefined}
+   */
+  update (id, data) {
+    return new Promise((resolve, reject) => {
+      this.index().partialUpdateObject({ objectID: id, ...data }, (error, data) => {
+        error ? reject(error) : resolve(data)
+      })
+    })
+  }
+
+  /**
+   * Delete a model from algolia.
+   * @param  {string} id  The id of the model
+   * @return {undefined}
+   */
+  delete (id) {
+    return new Promise((resolve, reject) => {
+      this.index().deleteObject(id, (error) => {
+        error ? reject(error) : resolve(true)
       })
     })
   }
