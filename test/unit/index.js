cd8806b932b18ce4eedf3638c9bd805c38791975
Joel Brubaker <pk.joel@gmail.com>
2017-03-29 17:21:38 -0700

    - removed old tests - updated test coverage to pull in bootstrapped app

--- test/unit/index.js
+++ test/unit/index.js
@@ -6,8 +6,12 @@ Function.prototype.bind = require('function-bind')
 const testsContext = require.context('./specs', true, /\.spec$/)
 testsContext.keys().forEach(testsContext)
 
+const services = require('src/config/services').default
+
+let app = require('app').boot(services)
+
 // require all src files except main.js for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-// const srcContext = require.context('src', true, /^\.\/(?!index(\.js)?$)/)
-// srcContext.keys().forEach(srcContext)
+const srcContext = require.context('src', true, /^\.\/(?!main(\.js)?$)/)
+srcContext.keys().forEach(srcContext)
--- test/unit/specs/app/drivers/ApiMockHttpDriver.spec.js
+++
@@ -1,9 +0,0 @@
-import ApiMockHttpDriver from 'src/app/drivers/ApiMockHttpDriver'
-
-describe('ApiMockHttpDriver', () => {
-  const mock = {}
-  it('should initialize', () => {
-    const driver = new ApiMockHttpDriver(mock)
-    driver.mock.should.deep.equal(mock)
-  })
-})
