d4587bcfce0e926be71c753ba6014aee3dde460d
Joel Brubaker <pk.joel@gmail.com>
2017-03-29 18:39:04 -0700

    - refactored crazy promises into something much simpler - refactored references to the ref() method

--- src/app/services/blueprint/drivers/firebase-driver.js
+++ src/app/services/blueprint/drivers/firebase-driver.js
@@ -11,30 +11,27 @@ export default class FirebaseBlueprintDriver {
   }
 
   /**
-   * Return a new key from the direbase database.
-   * @param  {string} location  A reference to a location.
-   * @return {string} The newly created key.
-   */
-  newKey (location) {
-    return this.firebase.database().ref(location).push().key
-  }
-
-  /**
    * Fetch data from the database by key.
-   * @param  {string} id  The key of the model to be accessed.
+   * @param  {string} key  The key of the model to be accessed.
    * @return {Object}  The model with the corresponding key.
    */
   fetch (key) {
     return new Promise((resolve, reject) => {
-      this.firebase.database().ref(`${this.location}/${key}`).once('value').then(snapshot => {
-        resolve({
-          id: snapshot.key,
-          ...snapshot.val()
-        })
-      }).catch(error => {
-        reject(error)
+      this.ref(key).once('value').then(snapshot => {
+        resolve({ id: snapshot.key, ...snapshot.val() })
       })
     })
+  }
+
+  /**
+   * Return a reference to the give model or a key for the given model.
+   * @param  {string} key  A child reference.
+   * @return {Reference}  A firebase reference.
+   */
+  ref (key) {
+    return key === undefined
+      ? this.firebase.database().ref(this.location)
+      : this.firebase.database().ref(`${this.location}/${key}`)
   }
 
   /**
@@ -43,19 +40,6 @@ export default class FirebaseBlueprintDriver {
    * @return {Promise} Resolves with the newly created resource
    */
   create (data) {
-    return new Promise((resolve, reject) => {
-      this.firebase.database().ref(`${this.location}`).push(data).then(ref => {
-        ref.once('value').then(snapshot => {
-          resolve({
-            id: snapshot.key,
-            ...snapshot.val()
-          })
-        }).catch(error => {
-          reject(error)
-        })
-      }).catch(error => {
-        reject(error)
-      })
-    })
+    return this.ref().push(data).then(({ key }) => this.fetch(key))
   }
 }
